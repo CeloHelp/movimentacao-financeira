40e5122364cb940f1756904609c696b6f4edb42e
Marcelo <marcelopacobello@gmail.com>
2025-03-27 00:03:32 -0300

    adicionar saldo feature

--- src/main/java/br/com/coderbank/movimentacaofinanceira/controller/ContaClienteControllerV1.java
+++ src/main/java/br/com/coderbank/movimentacaofinanceira/controller/ContaClienteControllerV1.java
@@ -1,6 +1,7 @@
 package br.com.coderbank.movimentacaofinanceira.controller;
 
 import br.com.coderbank.movimentacaofinanceira.dtos.request.ContaClienteRequestDTO;
+import br.com.coderbank.movimentacaofinanceira.dtos.request.DepositoRequestDTO;
 import br.com.coderbank.movimentacaofinanceira.dtos.response.ContaClienteResponseDTO;
 import br.com.coderbank.movimentacaofinanceira.entities.ContaCliente;
 import br.com.coderbank.movimentacaofinanceira.services.ContaClienteService;
@@ -8,13 +9,17 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/criarconta")
 public class ContaClienteControllerV1 {
     private final ContaClienteService service;
+    private final ContaClienteService contaClienteService;
 
-    public ContaClienteControllerV1(ContaClienteService service) {
+    public ContaClienteControllerV1(ContaClienteService service, ContaClienteService contaClienteService) {
         this.service = service;
+        this.contaClienteService = contaClienteService;
     }
 
     @PostMapping
@@ -32,4 +37,22 @@ public class ContaClienteControllerV1 {
 
         return ResponseEntity.ok(response);
     }
+
+    @PatchMapping("/{id}/deposito")
+    public ResponseEntity<ContaClienteResponseDTO> depositar(
+            @PathVariable UUID id,
+            @RequestBody @Valid DepositoRequestDTO depositoRequestDTO) {
+        ContaCliente contaAtualizada = contaClienteService.depositar(id, depositoRequestDTO.valor());
+
+        ContaClienteResponseDTO response = new ContaClienteResponseDTO(
+                contaAtualizada.getId(),
+                contaAtualizada.getTitular(),
+                contaAtualizada.getCpf(),
+                contaAtualizada.getNumeroConta(),
+                contaAtualizada.getAgencia(),
+                contaAtualizada.getSaldo()
+        );
+        return ResponseEntity.ok(response);
+    }
+
 }
